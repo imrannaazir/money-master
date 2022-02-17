4b7f70e9ccd4339caafc953270d800b3e43aa3de
imrannaazir <imrannaaziremon@gmail.com>
2022-02-17 11:29:19 +0600

    ux design updated

--- script.js
+++ script.js
@@ -1,20 +1,41 @@
-const calcuBtn = document.getElementById('calcu-btn').addEventListener('click', function () {
-    const incomeField = document.getElementById('income-field');
-    const foodField = document.getElementById('food-field');
-    const rentField = document.getElementById('rent-field');
-    const clothesField = document.getElementById('clothes-field');
+// input field variable
+const incomeField = document.getElementById('income-field');
+const foodField = document.getElementById('food-field');
+const rentField = document.getElementById('rent-field');
+const clothesField = document.getElementById('clothes-field');
+const saveField = document.getElementById('save-field')
 
+// balance text variable
+const totalExpenses = document.getElementById('total-expenses');
+const balance = document.getElementById('balance')
+const saveBalance = document.getElementById('save-balance')
+const remainingBalance = document.getElementById('remaining-balance')
+
+// button variable
+const calcuBtn = document.getElementById('calcu-btn')
+const saveBtn = document.getElementById('save-btn');
+
+
+
+//calculation button click hadeler addeded
+calcuBtn.addEventListener('click', function () {
     const expenses = parseInt(foodField.value) + parseInt(rentField.value) + parseInt(clothesField.value)
-
-
-    const totalExpenses = document.getElementById('total-expenses');
     totalExpenses.innerText = parseInt(totalExpenses.innerText) + expenses;
-    const balance = document.getElementById('balance')
     balance.innerText = parseInt(incomeField.value) - parseInt(totalExpenses.innerText)
 
 
-    incomeField.value = ''
+
     foodField.value = ''
     rentField.value = ''
     clothesField.value = ''
+})
+
+
+// save button click handeler addeded
+saveBtn.addEventListener('click', function () {
+    saveBalance.innerText = parseInt(incomeField.value) * parseInt(saveField.value) / 100;
+    remainingBalance.innerText = parseInt(balance.innerText) - parseInt(saveBalance.innerText)
+
+    incomeField.value = ''
+    saveField.value = ''
 })
